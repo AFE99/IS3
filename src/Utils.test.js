71007036a271e8b1e62f58b719b5b8fd9636822a
FeR35300 <Facevedo326@gmail.com>
2021-06-28 13:24:35 -0300

    corrigiendo test

--- src/Utils.test.js
+++ src/Utils.test.js
@@ -1,7 +1,7 @@
 import { producto } from './Utils';
 
 test('producto', () => {
-  expect(producto(2, 4)).toBe(9);
+  expect(producto(2, 4)).toBe(8);
   expect(producto(3, 4)).toBe(12);
   expect(producto(9, 0)).toBe(0);
 });
